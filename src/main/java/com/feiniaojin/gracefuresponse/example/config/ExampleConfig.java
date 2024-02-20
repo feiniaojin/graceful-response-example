bcd285f4e4eb0b0b7d2cca252f59704ca890f43b
qinyujie <943868899@qq.com>
2024-02-20 23:44:48 +0800

    3.5.1
    修复FastJson2下void返回值类型自动封装失效

--- src/main/java/com/feiniaojin/gracefuresponse/example/config/ExampleConfig.java
+++ src/main/java/com/feiniaojin/gracefuresponse/example/config/ExampleConfig.java
@@ -1,13 +1,22 @@
 package com.feiniaojin.gracefuresponse.example.config;
 
+import com.alibaba.fastjson2.JSONReader;
+import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.support.config.FastJsonConfig;
+import com.alibaba.fastjson2.support.spring.http.converter.FastJsonHttpMessageConverter;
 import org.hibernate.validator.HibernateValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Config类.
@@ -35,4 +44,17 @@ public class ExampleConfig implements WebMvcConfigurer {
     return validator;
   }
 
+  @Override
+  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+    //自定义配置...
+    FastJsonConfig config = new FastJsonConfig();
+    config.setDateFormat("yyyy-MM-dd HH:mm:ss");
+    config.setReaderFeatures(JSONReader.Feature.FieldBased, JSONReader.Feature.SupportArrayToBean);
+    config.setWriterFeatures(JSONWriter.Feature.WriteMapNullValue, JSONWriter.Feature.PrettyFormat);
+    converter.setFastJsonConfig(config);
+    converter.setDefaultCharset(StandardCharsets.UTF_8);
+    converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
+    converters.add(0, converter);
+  }
 }
